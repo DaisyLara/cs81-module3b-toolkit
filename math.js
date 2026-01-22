92906fe137cb618898f28961b8f13cae60a3e96e
Daisy Lara <lara_daisy_karina@student.smc.edu>
2026-01-21 22:58:10 -0800

    Added functions

---
+++ math.js
@@ -0,0 +1,28 @@
+//URL: https://github.com/DaisyLara/cs81-module3b-toolkit
+
+// Start by writing a simple function, like double(n).
+function double(n) {
+    return n * 2
+}
+console.log(double(12))
+
+function square(n) {
+    return n * n
+    
+}
+console.log(square(7))
+
+function isEven(n) {
+    return n === Number.isEven
+}
+console.log(isEven(7))
+
+function isOdd(n) {
+    return n === Number.isInteger
+}
+console.log(isOdd(7))
+
+function multiply(n) {
+    return n * 5
+}
+console.log(multiply(5))
